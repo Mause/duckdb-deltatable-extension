5dd7412803cfe6eb9b26448d880accb7f8669032
Elliana May <me@mause.me>
2022-10-02 18:13:16 +0800

    chore: tweak bridging function names

--- wrapper.c
+++ wrapper.c
@@ -1,14 +1,17 @@
+/*
+ * because we link twice (once to the rust library, and once to the duckdb library) we need a bridge to export the rust symbols
+ * this is that bridge
+ */
+
 #include "wrapper.h"
 
-const char* libtest_extension_version_v2();
-void libtest_extension_init_v2(void* db);
+const char* libtest_extension_version_rust(void);
+void libtest_extension_init_rust(void* db);
 
 DUCKDB_EXTENSION_API const char* libtest_extension_version() {
-    printf("hello world version\n");
-    return libtest_extension_version_v2();
+    return libtest_extension_version_rust();
 }
 
 DUCKDB_EXTENSION_API void libtest_extension_init(void* db) {
-    printf("hello world init\n");
-    libtest_extension_init_v2(db);
+    libtest_extension_init_rust(db);
 }
