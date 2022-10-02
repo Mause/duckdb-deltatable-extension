5f1bab614121148f227da27f56e39c0330aa0dda
Elliana May <me@mause.me>
2022-10-02 16:17:31 +0800

    refactor: move string tooling into standalone module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 use std::ffi::{c_void, CString};
-use std::mem::{size_of, transmute};
 use std::os::raw::c_char;
 use std::ptr::{addr_of_mut, null, null_mut};
-use std::slice;
-use std::sync::Once;
 
 use crate::duckly::{
     duckdb_add_replacement_scan, duckdb_close, duckdb_connect, duckdb_connection,
@@ -16,16 +13,11 @@ use crate::duckly::{
 
 mod duckly;
 mod error;
+mod strings;
 
 #[repr(C)]
 pub struct Wrapper {
     instance: *const u8,
-}
-
-#[repr(C)]
-struct duckdb_string_t {
-    length: u32,
-    data: *const c_char,
 }
 
 /// # Safety
@@ -55,30 +47,6 @@ pub extern "C" fn libtest_extension_init_v2(db: *mut u8) {
     }
 }
 
-const STRING_INLINE_LENGTH: i32 = 12;
-
-unsafe fn convert_string(val: *const c_void, idx: usize) -> CString {
-    assert!(idx >= 1);
-
-    let base_ptr = val.add((idx - 1) * size_of::<duckdb_string_t>());
-    let length_ptr = base_ptr as *const i32;
-    let length = *length_ptr;
-    if length <= STRING_INLINE_LENGTH {
-        let prefix_ptr = base_ptr.add(size_of::<i32>());
-        unsafe_string(prefix_ptr as *const u8, length)
-    } else {
-        let ptr_ptr = base_ptr.add(size_of::<i32>() * 2) as *const *const u8;
-        let data_ptr = *ptr_ptr;
-        unsafe_string(data_ptr, length)
-    }
-}
-
-unsafe fn unsafe_string(ptr: *const u8, len: i32) -> CString {
-    let slice = slice::from_raw_parts(ptr, len as usize);
-
-    CString::from_vec_unchecked(slice.to_vec())
-}
-
 #[no_mangle]
 pub extern "C" fn libtest_extension_version_v2() -> *const c_char {
     unsafe {
@@ -103,7 +71,7 @@ pub extern "C" fn libtest_extension_version_v2() -> *const c_char {
 
         let data = duckdb_vector_get_data(vect);
 
-        let res = convert_string(data, 1);
+        let res = strings::convert_string(data, 1);
 
         duckdb_destroy_data_chunk(&mut chunk);
         duckdb_destroy_result(&mut result);
@@ -111,16 +79,6 @@ pub extern "C" fn libtest_extension_version_v2() -> *const c_char {
         duckdb_disconnect(&mut connection);
         duckdb_close(&mut database);
 
-        versiony(res)
+        strings::static_version_string(res)
     }
-}
-
-static START: Once = Once::new();
-static mut VERSION_DATA: *const CString = 0 as *const CString;
-unsafe fn versiony(res: CString) -> *const c_char {
-    START.call_once(|| {
-        VERSION_DATA = transmute(Box::new(res));
-    });
-
-    (*VERSION_DATA).as_ptr()
 }
