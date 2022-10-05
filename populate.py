54efa5757808bb60395facf78fb613d1c517b376
Elliana May <me@mause.me>
2022-10-05 13:24:18 +0800

    test: fix test values

--- populate.py
+++ populate.py
@@ -1,10 +1,12 @@
-from datetime import date, datetime
 import pandas as pd
+from datetime import date
 from deltalake.writer import write_deltalake
+
+day = date(2022, 10, 4)
 df = pd.DataFrame({
     'x': [1, 2, 3],
     'other': [True, False, True],
     'third': ['foo', 'baz', 'bar'],
-    'd': [date.today(), date.today(), date.today()],
+    'd': [day, day, day],
 })
 write_deltalake('test/simple_table', df)
