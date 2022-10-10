a91cd3c31a15b97d76dc094255f783c00cf96d6f
Elliana May <me@mause.me>
2022-10-10 07:22:46 +0000

    feat: add more methods to structs

--- duckdb-extension-framework/src/data_chunk.rs
+++ duckdb-extension-framework/src/data_chunk.rs
@@ -1,14 +1,40 @@
-use crate::duckly::{duckdb_data_chunk, duckdb_data_chunk_get_vector, duckdb_data_chunk_set_size};
-use crate::Vector;
+use crate::duckly::{
+    duckdb_create_data_chunk, duckdb_data_chunk, duckdb_data_chunk_get_column_count,
+    duckdb_data_chunk_get_size, duckdb_data_chunk_get_vector, duckdb_data_chunk_reset,
+    duckdb_data_chunk_set_size, duckdb_logical_type, idx_t,
+};
+use crate::{LogicalType, Vector};
 
 pub struct DataChunk(duckdb_data_chunk);
 
 impl DataChunk {
+    #[doc = "Creates an empty DataChunk with the specified set of types."]
+    #[doc = ""]
+    #[doc = " types: An array of types of the data chunk."]
+    #[doc = " column_count: The number of columns."]
+    #[doc = " returns: The data chunk."]
+    pub fn duckdb_create_data_chunk(types: &[LogicalType]) -> Self {
+        let types: Vec<duckdb_logical_type> = types.iter().map(|x| x.typ).collect();
+        let mut types = types.into_boxed_slice();
+
+        Self(unsafe {
+            duckdb_create_data_chunk(types.as_mut_ptr(), types.len().try_into().unwrap())
+        })
+    }
     pub fn get_vector(&self, p0: u64) -> Vector {
         Vector::from(unsafe { duckdb_data_chunk_get_vector(self.0, p0) })
     }
     pub fn set_size(&self, size: u64) {
         unsafe { duckdb_data_chunk_set_size(self.0, size) };
+    }
+    pub fn chunk_reset(&self) {
+        unsafe { duckdb_data_chunk_reset(self.0) }
+    }
+    pub fn get_column_count(&self) -> idx_t {
+        unsafe { duckdb_data_chunk_get_column_count(self.0) }
+    }
+    pub fn get_size(&self) -> idx_t {
+        unsafe { duckdb_data_chunk_get_size(self.0) }
     }
 }
 
@@ -17,3 +43,11 @@ impl From<duckdb_data_chunk> for DataChunk {
         Self(ptr)
     }
 }
+
+// impl Drop for DataChunk {
+//     fn drop(&mut self) {
+//         unsafe {
+//             duckdb_destroy_data_chunk(&mut self.0);
+//         }
+//     }
+// }
--- duckdb-extension-framework/src/vector.rs
+++ duckdb-extension-framework/src/vector.rs
@@ -1,15 +1,14 @@
-use crate::duckly::{
-    duckdb_vector, duckdb_vector_assign_string_element_len, duckdb_vector_get_data, idx_t,
+use crate::{
+    duckly::{
+        duckdb_vector, duckdb_vector_assign_string_element,
+        duckdb_vector_assign_string_element_len, duckdb_vector_ensure_validity_writable,
+        duckdb_vector_get_column_type, duckdb_vector_get_data, duckdb_vector_get_validity, idx_t,
+    },
+    LogicalType,
 };
 use std::ffi::{c_char, c_void};
 
 pub struct Vector(duckdb_vector);
-
-impl Vector {
-    pub fn get_data(&self) -> *mut c_void {
-        unsafe { duckdb_vector_get_data(self.0) }
-    }
-}
 
 impl From<duckdb_vector> for Vector {
     fn from(ptr: duckdb_vector) -> Self {
@@ -18,6 +17,10 @@ impl From<duckdb_vector> for Vector {
 }
 
 impl Vector {
+    pub fn get_data(&self) -> *mut c_void {
+        unsafe { duckdb_vector_get_data(self.0) }
+    }
+
     /// Assigns a string element in the vector at the specified location.
     ///
     /// # Arguments
@@ -34,5 +37,28 @@ impl Vector {
         str_len: idx_t,
     ) {
         duckdb_vector_assign_string_element_len(self.0, index, str_, str_len);
+    }
+
+    /// Assigns a string element in the vector at the specified location.
+    ///
+    /// # Arguments
+    ///  * `index` - The row position in the vector to assign the string to
+    ///  * `str` - The null-terminated string"]
+    ///
+    ///     #[doc = "Assigns a string element in the vector at the specified location."]
+    ///
+    /// # Safety
+    /// .
+    pub unsafe fn assign_string_element(&self, index: idx_t, str_: *const c_char) {
+        duckdb_vector_assign_string_element(self.0, index, str_);
+    }
+    pub fn get_column_type(&self) -> LogicalType {
+        unsafe { LogicalType::from(duckdb_vector_get_column_type(self.0)) }
+    }
+    pub fn get_validity(&self) -> *mut u64 {
+        unsafe { duckdb_vector_get_validity(self.0) }
+    }
+    pub fn ensure_validity_writable(&self) {
+        unsafe { duckdb_vector_ensure_validity_writable(self.0) };
     }
 }
