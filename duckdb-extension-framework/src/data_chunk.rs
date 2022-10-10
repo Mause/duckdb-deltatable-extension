49226508e6ad899f398886695c1e34efab48ba85
Elliana May <me@mause.me>
2022-10-10 07:29:57 +0000

    chore: fix ownership when dropping

--- duckdb-extension-framework/src/data_chunk.rs
+++ duckdb-extension-framework/src/data_chunk.rs
@@ -1,11 +1,14 @@
 use crate::duckly::{
     duckdb_create_data_chunk, duckdb_data_chunk, duckdb_data_chunk_get_column_count,
     duckdb_data_chunk_get_size, duckdb_data_chunk_get_vector, duckdb_data_chunk_reset,
-    duckdb_data_chunk_set_size, duckdb_logical_type, idx_t,
+    duckdb_data_chunk_set_size, duckdb_destroy_data_chunk, duckdb_logical_type, idx_t,
 };
 use crate::{LogicalType, Vector};
 
-pub struct DataChunk(duckdb_data_chunk);
+pub struct DataChunk {
+    ptr: duckdb_data_chunk,
+    owned: bool,
+}
 
 impl DataChunk {
     /// Creates an empty DataChunk with the specified set of types.
@@ -16,37 +19,39 @@ impl DataChunk {
         let types: Vec<duckdb_logical_type> = types.iter().map(|x| x.typ).collect();
         let mut types = types.into_boxed_slice();
 
-        Self(unsafe {
+        let ptr = unsafe {
             duckdb_create_data_chunk(types.as_mut_ptr(), types.len().try_into().unwrap())
-        })
+        };
+
+        Self { ptr, owned: true }
     }
     pub fn get_vector(&self, p0: u64) -> Vector {
-        Vector::from(unsafe { duckdb_data_chunk_get_vector(self.0, p0) })
+        Vector::from(unsafe { duckdb_data_chunk_get_vector(self.ptr, p0) })
     }
     pub fn set_size(&self, size: u64) {
-        unsafe { duckdb_data_chunk_set_size(self.0, size) };
+        unsafe { duckdb_data_chunk_set_size(self.ptr, size) };
     }
     pub fn chunk_reset(&self) {
-        unsafe { duckdb_data_chunk_reset(self.0) }
+        unsafe { duckdb_data_chunk_reset(self.ptr) }
     }
     pub fn get_column_count(&self) -> idx_t {
-        unsafe { duckdb_data_chunk_get_column_count(self.0) }
+        unsafe { duckdb_data_chunk_get_column_count(self.ptr) }
     }
     pub fn get_size(&self) -> idx_t {
-        unsafe { duckdb_data_chunk_get_size(self.0) }
+        unsafe { duckdb_data_chunk_get_size(self.ptr) }
     }
 }
 
 impl From<duckdb_data_chunk> for DataChunk {
     fn from(ptr: duckdb_data_chunk) -> Self {
-        Self(ptr)
+        Self { ptr, owned: false }
     }
 }
 
-// impl Drop for DataChunk {
-//     fn drop(&mut self) {
-//         unsafe {
-//             duckdb_destroy_data_chunk(&mut self.0);
-//         }
-//     }
-// }
+impl Drop for DataChunk {
+    fn drop(&mut self) {
+        if self.owned {
+            unsafe { duckdb_destroy_data_chunk(&mut self.ptr) };
+        }
+    }
+}
