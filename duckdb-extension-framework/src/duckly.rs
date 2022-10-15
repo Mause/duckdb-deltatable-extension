f881e7b4ebdc308852e96aa4be3a8271ba5c64ac
Elliana May <me@mause.me>
2022-10-15 12:49:24 +0800

    refactor: remove inline framework

--- duckdb-extension-framework/src/duckly.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(unused)]
-#![allow(improper_ctypes)]
-#![allow(clippy::upper_case_acronyms)]
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
