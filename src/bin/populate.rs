648421df6236d8e248bed7e602ec10884bb5f40f
Elliana May <me@mause.me>
2023-04-05 14:52:20 +0800

    get new populate script to a working state

--- src/bin/populate.rs
+++ src/bin/populate.rs
@@ -1,51 +1,106 @@
-use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+use deltalake::arrow::compute::kernels::cast_utils::Parser;
+use deltalake::arrow::datatypes::Date32Type;
+use deltalake::DeltaTableError::{Generic, NotATable};
 use deltalake::{
     action::{Action, DeltaOperation, SaveMode},
     arrow::{
-        array::{BooleanArray, Date64Array, Int32Array, StringArray},
+        array::{BooleanArray, Date32Array, Int32Array, StringArray},
         datatypes::{DataType, Field, Schema},
         record_batch::RecordBatch,
     },
     writer::{DeltaWriter, RecordBatchWriter},
-    DeltaOps, SchemaDataType,
+    DeltaOps, DeltaTable, SchemaDataType,
 };
 use std::sync::Arc;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let batch = create_record_batch();
+
+    let mut table = obtain_table().await;
+
+    println!("table version: {}", table.version());
+
+    let partition_columns = vec!["x".to_string()];
+
+    let mut writer = RecordBatchWriter::for_table(&table).expect("for_table");
+    writer.write(batch).await.expect("write");
+    let actions: Vec<Action> = writer
+        .flush()
+        .await?
+        .iter()
+        .map(|add| Action::add(add.clone()))
+        .collect();
+    let mut transaction = table.create_transaction(None);
+    transaction.add_actions(actions);
+    transaction
+        .commit(
+            Some(DeltaOperation::Write {
+                mode: SaveMode::Append,
+                partition_by: Some(partition_columns.clone()),
+                predicate: None,
+            }),
+            None,
+        )
+        .await?;
+
+    println!("done");
+
+    Ok(())
+}
+
+async fn obtain_table() -> DeltaTable {
+    match mk_ops().await.load().await {
+        Err(err) => match err {
+            Generic(msg) => {
+                panic!("generic error: {:?}", msg)
+            }
+            NotATable(msg) => {
+                println!("NotATable, creating table: {:?}", msg);
+                create_table().await
+            }
+            _ => {
+                panic!("error: {:?}", err);
+            }
+        },
+        Ok(table) => {
+            println!("table loaded successfully");
+            table.0
+        }
+    }
+}
+
+fn create_record_batch() -> RecordBatch {
     let schema = Schema::new(vec![
         Field::new("x", DataType::Int32, false),
         Field::new("other", DataType::Boolean, false),
         Field::new("third", DataType::Utf8, false),
-        Field::new("d", DataType::Date64, false),
+        Field::new("d", DataType::Date32, false),
     ]);
 
-    let day = NaiveDateTime::new(
-        "2022-10-04".parse::<NaiveDate>()?,
-        NaiveTime::from_num_seconds_from_midnight_opt(0, 0).unwrap(),
-    )
-    .timestamp_millis();
+    let day = Date32Type::parse("2022-10-04");
 
-    let batch = RecordBatch::try_new(
+    RecordBatch::try_new(
         Arc::new(schema),
         vec![
             Arc::new(Int32Array::from(vec![1, 2, 3])),
             Arc::new(BooleanArray::from(vec![true, false, true])),
             Arc::new(StringArray::from(vec!["foo", "baz", "bar"])),
-            Arc::new(Date64Array::from(vec![day, day, day])),
+            Arc::new(Date32Array::from(vec![day, day, day])),
         ],
     )
-    .unwrap();
+    .unwrap()
+}
 
-    let ops = DeltaOps::try_from_uri("test/simple_table")
+async fn mk_ops() -> DeltaOps {
+    DeltaOps::try_from_uri("test/simple_table")
         .await
-        .expect("try_from_uri");
+        .expect("try_from_uri")
+}
 
-    // if let Ok(table) = ops.0.load().await {
-
-    // }
-
-    let mut table = ops
+async fn create_table() -> DeltaTable {
+    mk_ops()
+        .await
         .create()
         .with_table_name("my_table")
         .with_column(
@@ -73,31 +128,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             None,
         )
         .await
-        .expect("create table");
-    assert_eq!(table.version(), 0);
-
-    let partition_columns = vec!["x".to_string()];
-
-    let mut writer = RecordBatchWriter::for_table(&table).expect("for_table");
-    writer.write(batch).await?;
-    let actions: Vec<Action> = writer
-        .flush()
-        .await?
-        .iter()
-        .map(|add| Action::add(add.clone()))
-        .collect();
-    let mut transaction = table.create_transaction(None);
-    transaction.add_actions(actions);
-    transaction
-        .commit(
-            Some(DeltaOperation::Write {
-                mode: SaveMode::Append,
-                partition_by: Some(partition_columns.clone()),
-                predicate: None,
-            }),
-            None,
-        )
-        .await?;
-
-    Ok(())
+        .expect("create table")
 }
