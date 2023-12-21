2d66bd480b2bcd03396e5e9961612d375eb65662
Elliana May <me@mause.me>
2023-12-21 16:49:34 +0800

    rejig with macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
-use duckdb::ffi::{duckdb_database, duckdb_library_version};
 use duckdb::Connection;
+use duckdb_loadable_macros::duckdb_entrypoint;
+use libduckdb_sys as ffi;
 use std::{
     error::Error,
     ffi::{c_char, c_void},
@@ -14,19 +15,8 @@ mod types;
 /// Init hook for DuckDB, registers all functionality provided by this extension
 /// # Safety
 /// .
-#[no_mangle]
-pub unsafe extern "C" fn deltatable_init_rust(db: *mut c_void) {
-    init(db.cast()).expect("init failed");
-}
-
-unsafe fn init(db: duckdb_database) -> Result<(), Box<dyn Error>> {
-    let connection = Connection::from_cpp(db)?;
-    connection.register_table_function::<DeltaFunction>("read_delta")?;
+#[duckdb_entrypoint]
+pub fn deltatable_init_rust(conn: Connection) -> Result<(), Box<dyn Error>> {
+    conn.register_table_function::<DeltaFunction>("read_delta")?;
     Ok(())
-}
-
-/// Version hook for DuckDB, indicates which version of DuckDB this extension was compiled against
-#[no_mangle]
-pub extern "C" fn deltatable_version_rust() -> *const c_char {
-    unsafe { duckdb_library_version() }
 }
