bfb821b988ac022775c7d6a5270906ac67fb2250
Elliana May <me@mause.me>
2023-12-28 22:47:18 +0800

    add cpp wrapper

---
+++ src/deltatable_extension.cpp
@@ -0,0 +1,29 @@
+#define DUCKDB_EXTENSION_MAIN
+
+#include "deltatable_extension.hpp"
+#include "duckdb.hpp"
+
+extern "C" {
+const char *deltatable_version_rust(void);
+void deltatable_init_rust(void *db);
+
+DUCKDB_EXTENSION_API void
+duckdb_deltatable_extension_init(duckdb::DatabaseInstance &db) {
+  deltatable_init_rust((void *)&db);
+}
+
+DUCKDB_EXTENSION_API const char *duckdb_deltatable_extension_version() {
+  return deltatable_version_rust();
+}
+};
+
+#ifndef DUCKDB_EXTENSION_MAIN
+#error DUCKDB_EXTENSION_MAIN not defined
+#endif
+
+void duckdb::DeltatableExtension::Load(DuckDB &db) {
+  DuckDB *ptr = &db;
+  deltatable_init_rust((void *)ptr);
+}
+
+std::string duckdb::DeltatableExtension::Name() { return "deltatable"; }
